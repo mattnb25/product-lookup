e39d50b56fe73424efa9e3307009969d5091d18f
mattnb25 <212289972+mattnb25@users.noreply.github.com>
2025-12-27 21:36:15 +0000

    refactor to fetch last modified date

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,16 +1,17 @@
-import { statSync, readFileSync } from 'node:fs';
 import { parseCsvToBarcodeMap } from '$lib/csvParser.js';
 
-export function load() {
-    const stats = statSync('./static/home-tan.csv');
+export async function load({ fetch }) {
+    const res = await fetch('/home-tan.csv');
+    const csvText = await res.text();
 
-    const lastModified = stats.mtime.toLocaleDateString('en-GB', {
+    const lastModifiedHeader = res.headers.get('last-modified');
+    const lastModifiedDate = lastModifiedHeader ? new Date(lastModifiedHeader) : new Date();
+    const lastModified = lastModifiedDate.toLocaleDateString('en-GB', {
         day: 'numeric',
         month: 'long',
         year: 'numeric'
     });
 
-    const csvText = readFileSync('./static/home-tan.csv', 'utf-8');
     const barcodeData = parseCsvToBarcodeMap(csvText);
 
     return { lastModified, barcodeData };
